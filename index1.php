5599fb31fb1fb9042d184f95b9c9dc78a3d78f74
olisaxh <olisaxhh01@gmail.com>
2025-11-19 17:26:21 +0100

    DTRGFHJ

---
+++ index1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY FIRST PHP LESSON</title>
+</head>
+<body>
+    <h1>MY FIRST PHP LESSON</h1>
+    <?php
+      echo "HELLO WORLD"
+    ?>
+</body>
+</html>
