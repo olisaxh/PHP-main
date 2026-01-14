324eddd1bd69108d41e34ea77c89c95d73f09617
olisaxh <olisaxhh01@gmail.com>
2026-01-14 17:45:44 +0100

    r4gt

---
+++ lesson9/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$user= "root";
+$pass= "";
+$server= "localhost";
+$dbname= "db";
+
+try{
+    $conn = new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
+}catch(PDOExcpetion $e) {
+    echo "error" . $e->getmessage();
+}
+ 
+
+
+?>
---
+++ lesson9/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="name" placeholder="Name"><br>
+         <input type="text" name="surname" placeholder="Surname"><br>
+          <input type="email" name="email" placeholder="Email"><br>
+           <button typw="submit" name="Submit">add</button>
+
+
+
+
+    </form>
+</body>
+</html>
